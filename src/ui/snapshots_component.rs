3aaba6f0d010a2cf9216cefaf325f8de7467f861
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-05-18 18:10:55 +0200

    feat(ui): Add simple border and size for the popup.

--- src/ui/snapshots_component.rs
+++ src/ui/snapshots_component.rs
@@ -1,6 +1,15 @@
 use crate::ui::theme::TableColors;
 
-use ratatui::{Frame, layout::Rect};
+use ratatui::{
+    Frame,
+    layout::{Constraint, Direction, Flex, Layout, Margin, Rect},
+    prelude::Style,
+    style::Stylize,
+    text::Line,
+    widgets::{Block, BorderType, Clear, Paragraph, Wrap},
+};
+
+use crate::util::popup_area;
 
 /// A component that handles the snapshots
 #[derive(Debug)]
@@ -22,9 +31,19 @@ impl SnapshotsComponent {
     }
 
     /// Renders the popup
-    pub fn render(&self, _frame: &mut Frame, _area: Rect, _colors: &TableColors) {
+    pub fn render(&self, frame: &mut Frame, area: Rect, colors: &TableColors) {
         if !self.display {
             return;
         }
+
+        let block = Block::bordered()
+            .border_type(BorderType::Plain)
+            .border_style(Style::new().fg(colors.footer_border_color))
+            .bg(colors.buffer_bg)
+            .title("Snapshotting");
+
+        let area = popup_area(area, 4, 5);
+        frame.render_widget(Clear, area);
+        frame.render_widget(block, area);
     }
 }
